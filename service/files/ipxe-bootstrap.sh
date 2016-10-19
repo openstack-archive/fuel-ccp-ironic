35b8c9fbc83a74267ae295fe56d1da621c4e53ed
Andrey Shestakov <ashestakov@mirantis.com>
2016-10-20 01:07:14 +0300

    Add Ironic iPXE support
    Change-Id: Id9793e92ddf5b46e2913bf19f56df235c1f4bb87

---
+++ service/files/ipxe-bootstrap.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p /var/lib/ironic/httpboot
+chown -R ironic: /var/lib/ironic
--- service/files/pxe-bootstrap.sh
+++ service/files/pxe-bootstrap.sh
@@ -6,6 +6,7 @@ cp \
     /usr/lib/PXELINUX/pxelinux.0 \
     /usr/lib/syslinux/modules/bios/chain.c32 \
     /usr/lib/syslinux/modules/bios/ldlinux.c32 \
+    /usr/lib/ipxe/undionly.kpxe \
     /var/lib/ironic/tftpboot
 
-chown -R ironic:ironic /var/lib/ironic
+chown -R ironic: /var/lib/ironic
